3405e16ee26353545591d6cabd50977d0bce886b
parker84 <brydonparker4@gmail.com>
2023-02-26 10:01:18 -0500

    adding ability to upload a list of followers and get engagement metrics

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 from PIL import Image
+import pandas as pd
 from scraper import Scraper, ScrapeEngagement
+import coloredlogs, logging
+from decouple import config
+logger = logging.getLogger(__name__)
+coloredlogs.install(level=config('LOG_LEVEL'))
 
 # ----helpers
 @st.experimental_memo
@@ -37,9 +42,16 @@ with st.form("my_form"):
     if min_follower_count_to_get_engagement_metrics > 0 and get_engagement_metrics == False:
         st.warning(f"You've indicated a min follower count ({min_follower_count_to_get_engagement_metrics}), but you haven't checked off the box to get engagement metrics, so we no engagement metrics will be added. If you want them added, please check the Engagement Metrics box.")
     get_tweets = st.checkbox("Get Their Tweets")
-
     # Every form must have a submit button.
     submitted = st.form_submit_button("Generate Leads")
+
+with st.form('upload_form'):
+    st.markdown('Already have the followers scraped and want to add engagement metrics 👇🏻')
+    username_for_upload = st.text_input('Enter a Username', '@parker_brydon')
+    username_for_upload = username_for_upload.strip('@')
+    min_follower_count_to_get_engagement_metrics_upload_form = st.slider('Min Follower Count for Engagement Metrics', min_value=0, max_value=100000, step=1000)
+    uploaded_file = st.file_uploader("Upload a csv of followers to add engagement metrics", type='csv')
+    submitted_upload_form = st.form_submit_button('Get Engagement Metrics')
 
 if submitted:
     st.success(
@@ -76,6 +88,7 @@ if submitted:
         with st.spinner(text=f"Scraping `{username}`'s followers"):
             followers_df = scraper.scrape_followers_for_user()
         if get_engagement_metrics:
+            logger.info('Getting engagement metrics for followers')
             with st.spinner(text=f"Scraping engagement metrics_df for followers, with follower counts >= {min_follower_count_to_get_engagement_metrics}"):
                 metrics_df = engagement_scraper.scrape_metrics_for_last_hundred_tweets_for_users(
                     followers_df[followers_df.followers_count >= min_follower_count_to_get_engagement_metrics],
@@ -107,4 +120,26 @@ if submitted:
         )
 
 
-
+if submitted_upload_form:
+    followers_df = pd.read_csv(uploaded_file)
+    followers_df = followers_df[
+        [isinstance(val, int) for val in followers_df['followers_count']]
+    ].infer_objects()
+    followers_df['user_id'] = followers_df['user_id'].astype(int)
+    followers_df['followers_count'] = followers_df['followers_count'].astype(int)
+    engagement_scraper = ScrapeEngagement(username_for_upload)
+    with st.spinner(text=f"Scraping engagement metrics_df for followers, with follower counts >= {min_follower_count_to_get_engagement_metrics_upload_form}"):
+        metrics_df = engagement_scraper.scrape_metrics_for_last_hundred_tweets_for_users(
+            followers_df[followers_df.followers_count >= min_follower_count_to_get_engagement_metrics_upload_form],
+            followers_or_followings='followers'
+        )
+    followers_df = followers_df.merge(metrics_df, on='username', how='left')
+    st.dataframe(followers_df)
+    followers_df.to_csv(f'./data/{username_for_upload}_followers_df.csv')
+    csv = convert_df(followers_df)
+    st.download_button(
+        "Press to Download",
+        csv,
+        f"{username_for_upload}_followers.csv",
+        "text/csv"
+    )
